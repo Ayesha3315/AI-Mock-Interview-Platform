6d2a79d0a2485e9861faee20a43777051abbd822
Ayesha Fatima <ayesha.fatima3315@gmail.com>
2026-08-19 15:03:40 +0530

    feat(backend):Added RoleController

---
+++ backend/src/main/java/com/interview/backend/controller/RoleController.java
@@ -0,0 +1,28 @@
+package com.interview.backend.controller;
+
+
+import com.interview.backend.dto.RoleResponseDTO;
+import com.interview.backend.service.RoleService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/roles")
+public class RoleController {
+
+    private final RoleService roleService;
+
+    public RoleController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<RoleResponseDTO>> getAllRoles() {
+        List<RoleResponseDTO> roles = roleService.getAllRoles();
+        return ResponseEntity.ok(roles);
+    }
+}
